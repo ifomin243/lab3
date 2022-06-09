87baa373232353d74c7ccdfb9f8f4206ea9c68f5
ifomin243 <aftzKfinWs78d3KPV5fY>
2022-06-09 11:35:52 +0200

    добавлены комментарии в класс Sphere

--- Model/Sphere.cs
+++ Model/Sphere.cs
@@ -23,16 +23,19 @@ namespace Model
                 r = value;
             }
         }
-
+        // конструктор класса Parallelepiped по умолчанию
         public Sphere() { }
+        // конструктор класса Parallelepiped с параметрами вариант 1
         public Sphere(double r)
         {
             Side = r;
         }
+        // конструктор класса Parallelepiped с параметрами вариант 2
         public Sphere(string r)
         {
             Side = InputValidation(r);
         }
+        // функция подсчета объёма
         public override double GetVolume()
         {
             return Math.Round(4 / 3.0 * (Math.PI * (Side * Side * Side)), 2);
